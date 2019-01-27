c53e1344ac1a9ba021385cdfb302224a07bc1e2f
bituhh <victor@oliveira.org.uk>
2019-01-27 09:22:17 +0000

    p5.js Project

---
+++ perceptron.js
@@ -0,0 +1,25 @@
+function perceptron() {
+  this.weights = new Array( 2 );
+  for ( var i = 0; i < weights.length; i++ ) {
+    weights[ i ] = random( -1, 1 );
+  }
+  console.log( weights );
+
+
+
+  this.createWeights = function( size ) {
+    temp = new Array( size );
+    for ( var i = 0; i < temp.length; i++ ) {
+      temp[ i ] =
+    }
+  }
+
+  this.update = function() {
+    console.log( 'test' );
+  };
+
+  this.show = function() {
+
+  };
+
+}
